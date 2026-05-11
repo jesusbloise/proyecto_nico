a95d210b19aaf693f1b956de26f76c256629bd87
jesus bloise <jesusbloise@gmail.com>
2026-05-11 11:28:12 -0400

    Support DATABASE_URL for Render Postgres

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,9 +1,21 @@
 import { Pool } from "pg";
 
-export const pool = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT || 5432),
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-});
+const connectionString = process.env.DATABASE_URL;
+
+export const pool = connectionString
+  ? new Pool({
+      connectionString,
+      ssl:
+        process.env.PGSSL === "false"
+          ? false
+          : {
+              rejectUnauthorized: false,
+            },
+    })
+  : new Pool({
+      host: process.env.PGHOST,
+      port: Number(process.env.PGPORT || 5432),
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+    });
